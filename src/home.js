e693e18e678e2c5d86105f996d14f765a69f3f29
unknown <dzmidou95@gmail.com>
2021-03-10 22:12:31 +0100

    create component & add styles & events & state and hooks

---
+++ src/home.js
@@ -0,0 +1,27 @@
+import { waitForElement } from "@testing-library/react";
+import {useState} from 'react'
+
+const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
+    return ( 
+        <div className="home">
+            {
+blogs.map((blog)=>(
+    <div className="blog-preview" key={blog.id}>
+<h2>{blog.title}</h2>
+<h4>Written by: {blog.author}</h4>
+
+    </div>
+))
+            }
+          
+                   </div>
+     );
+}
+ 
+export default Home;
