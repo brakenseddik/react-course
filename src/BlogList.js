026e57345bafe2684a3c6993ea4cb44049672814
unknown <dzmidou95@gmail.com>
2021-03-11 21:53:44 +0100

    Pros classic

---
+++ src/BlogList.js
@@ -0,0 +1,21 @@
+const BlogList = (props) => {
+const blogs=props.blogs;
+const title=props.title;
+
+
+    return ( 
+        
+        <div className="blog-list">
+            <h2>{title}</h2>
+        {blogs.map((blog)=>(
+<div className="blog-preview" key={blog.id}>
+<h2>{blog.title}</h2>
+<h4>Written by: {blog.author}</h4>
+</div>
+)) }
+      
+ </div>
+     );
+}
+ 
+export default BlogList;
