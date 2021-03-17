2ea819b410dc802a90f0bcfed6e9ca5bc69858de
unknown <dzmidou95@gmail.com>
2021-03-17 22:02:12 +0100

    handeling exceptions

--- src/home.js
+++ src/home.js
@@ -5,24 +5,37 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setisLoading] = useState(true);
+    const [error, seterror] = useState(null)
      
      useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
       .then(res => {
+        console.log(res);
+        if (!res.ok) {
+          throw Error('could not fetch the data')
+          
+        }
         return res.json();
       })
       .then(data => {
         setBlogs(data);
-        setisLoading(false)
+        setisLoading(false);
+        seterror(null)
+      })
+      .catch(err=>{
+        setisLoading(false);
+        seterror(err.message);
+
       })
     }, 1000); 
   }, []);
 
     return ( 
       <div className="home">
+        {error && <div>{error}</div> }
         {isLoading && <div>Loading data ...</div> }
-   { blogs &&  <BlogList blogs={blogs} title="All Blogs" />}    
+        { blogs &&  <BlogList blogs={blogs} title="All Blogs" />}    
    {/* <button onClick={()=>{setname("seddik")}}> change name</button> */}
       </div>
      );
